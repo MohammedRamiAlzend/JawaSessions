d839a951e6a353f72ae46f106fd67744f1800be0
MohammedRamiAlzend <162128732+MohammedRamiAlzend@users.noreply.github.com>
2025-02-24 22:03:19 +0300

    reset main

--- JawaSessions/JawaSessions.cpp
+++ JawaSessions/JawaSessions.cpp
@@ -1,27 +1,6 @@
-// JawaSessions.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 using namespace std;
 int main()
 {
-	double  firstNumber = 11;
-	double secondeNumber = 21;
-
-	cout << firstNumber + secondeNumber << endl;
-	cout << firstNumber - secondeNumber << endl;
-	cout << firstNumber * secondeNumber << endl;
-	cout << firstNumber / secondeNumber << endl;
-	cout << ((int)firstNumber % (int)secondeNumber) << endl;
+	
 }
-/*
-Basics Arthmatic Operators
-
-+
-*
--
-/
-%
-
-
-*/
