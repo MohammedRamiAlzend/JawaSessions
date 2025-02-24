a3cd9f5ded64fda23ee20c8fa0ed9decb1925114
MohammedRamiAlzend <162128732+MohammedRamiAlzend@users.noreply.github.com>
2025-02-24 22:41:10 +0300

    end of cin

--- JawaSessions/JawaSessions.cpp
+++ JawaSessions/JawaSessions.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 int main()
 {
-	
+	//string firstName, lastName;
+	//cout << "Enter your first name:";
+	//cin >> firstName;
+	//cout << "Enter your last name:";
+	//cin >> lastName;
+	//cout << "Your Full Name:" << firstName + "\t" +	lastName << endl;
+	int firstNumber;
+	int secondNumber;
+	cout << "Enter first number:";
+	cin >> firstNumber;
+	cout << "Enter second number:";
+	cin >> secondNumber;
+	cout << "result  = " <<  firstNumber +secondNumber << endl;
 }
