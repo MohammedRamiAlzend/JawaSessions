46c93805afc22a5a61d2d67354c843459cb38818
jawa mehrez <jawamehrez43@gmail.com>
2025-03-05 23:57:42 +0300

    hi

--- JawaSessions/JawaSessions.cpp
+++ JawaSessions/JawaSessions.cpp
@@ -2,51 +2,7 @@
 using namespace std;
 int main()
 {
-	/*int value;
-	cout << "Enter a value to indicate if it positive or not" << endl;
-	cin >> value;
-	if (value > 0)
-	{
-		cout << "positive";
-	}
-	if (value < 0)
-	{
-		cout << "negative";
-	}
-	if (value == 0)
-	{
-		cout << "Zero";
-	}*/
-
-
-	//cout << "Enter points:" << endl;
-	//int point;
-	//cin >> point;
-	//if (point >= 50)
-	//{
-	//	cout << "pass";
-	//}
-	//else
-	//{
-	//	cout << "fail";
-	//}
-
-	/*
-	* and &&
-	* t + t = t
-	* t + f = f
-	* f + t = f
-	* f + f = f
-	*
-	* or ||
-	* t + t = t
-	* t + f = t
-	* f + t = t
-	* f + f = f
-	*/
-	string dayName;
-	cout << "Enter day:" << endl;
-	cin >> dayName;//Sun
+	string monthename;
 
 	if (dayName == "sun" 
 		|| dayName == "Sun" || 
