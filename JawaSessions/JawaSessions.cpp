8ae1b17684eed988a611729c85d1818c07d57ca6
rami <ramialzend@gmail.com>
2025-04-07 23:10:06 +0300

    factorial example

--- JawaSessions/JawaSessions.cpp
+++ JawaSessions/JawaSessions.cpp
@@ -1,20 +1,23 @@
-// JawaSessions.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+using namespace std;
+/*
+    x => 15,14,13,12,11, .... , 1,0
+ */
 int main()
 {
-    std::cout << "Hello8 !\n";
+  // n! 
+  // 5! = 5 * 4 * 3 * 2 * 1
+  // 
+
+  int n ;
+  cout<<"Enter a number : ";
+  cin >> n;
+  int s = n;
+  while (n>1)
+  {
+      s = s * --n;
+  }
+
+  cout<<s<<endl;
+  cout<<n<<endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
