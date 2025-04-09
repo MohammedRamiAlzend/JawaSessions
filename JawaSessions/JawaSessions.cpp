65aa863030cb74a3d6adc432fcf77902227843ea
rami <ramialzend@gmail.com>
2025-04-09 22:56:13 +0300

    get number of factorials number

--- JawaSessions/JawaSessions.cpp
+++ JawaSessions/JawaSessions.cpp
@@ -5,19 +5,42 @@ using namespace std;
  */
 int main()
 {
-  // n! 
-  // 5! = 5 * 4 * 3 * 2 * 1
-  // 
+    // int number;
+    // cout << "enter number:" << endl;
+    // cin >> number;
+    // while (number > 1)
+    // {
+    //   if (number % 2 == 1 || number == 2)
+    //   {
+    //     cout << "the number is  prime" << endl;
+    
+    //   }
+    //   else
+    //   {
+    //     cout << "the number is not prime" << endl;
+    //   }
+    // }
 
-  int n ;
-  cout<<"Enter a number : ";
-  cin >> n;
-  int s = n;
-  while (n>1)
-  {
-      s = s * --n;
-  }
+    //10 : 1,2,5 => 3
+    //15 : 1,3,5 => 3
+    //14 : 1,2,7 => 3
+    //20 : 1,2,4,5,10 =>5
+    //30 : 1,2,3,5,6,10,15 => 7
+    int number = 10;//1,2,5
+    int resultCounter = 0;
+    int start = 1;
+    int end = number / 2;
+    while (start <= end)
+    {
+        if(number%start == 0)
+        {
+            resultCounter++;
+        }
+        cout<<resultCounter<<endl;
+        start++;
+    }
 
-  cout<<s<<endl;
-  cout<<n<<endl;
+
+    
+
 }
