7b674f3a67d71c22d687b6213bb4ea5fd5ebcf1b
rami <ramialzend@gmail.com>
2025-04-09 23:10:17 +0300

    prime number algorthim

--- JawaSessions/JawaSessions.cpp
+++ JawaSessions/JawaSessions.cpp
@@ -5,40 +5,27 @@ using namespace std;
  */
 int main()
 {
-    // int number;
-    // cout << "enter number:" << endl;
-    // cin >> number;
-    // while (number > 1)
-    // {
-    //   if (number % 2 == 1 || number == 2)
-    //   {
-    //     cout << "the number is  prime" << endl;
-    
-    //   }
-    //   else
-    //   {
-    //     cout << "the number is not prime" << endl;
-    //   }
-    // }
-
-    //10 : 1,2,5 => 3
-    //15 : 1,3,5 => 3
-    //14 : 1,2,7 => 3
-    //20 : 1,2,4,5,10 =>5
-    //30 : 1,2,3,5,6,10,15 => 7
-    int number = 10;//1,2,5
-    int resultCounter = 0;
-    int start = 1;
-    int end = number / 2;
+    int number= 113;
+    int start= 2;
+    bool isPrime = true;
+    int end= number/2;
     while (start <= end)
     {
-        if(number%start == 0)
+        if(number % start == 0)
         {
-            resultCounter++;
+            isPrime = false;
         }
-        cout<<resultCounter<<endl;
         start++;
     }
+    if(isPrime == false)
+    {
+        cout<<"not a prime number"<<endl;
+    }
+    else
+    {
+        cout<<"prime number"<<endl;
+    }
+
 
 
     
