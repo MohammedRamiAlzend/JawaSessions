4e39d8268a140a04bac1dc4ddba1477f0c26d828
Rami <Rami@DESKTOP-M499S5E>
2025-07-08 12:08:39 +0300

    print the avg of numbers [1,999]

--- JawaSessions/JawaSessions.cpp
+++ JawaSessions/JawaSessions.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 using namespace std;
-/*
-    x => 15,14,13,12,11, .... , 1,0
- */
+
 int main()
 {
-    //[1,10]=>1,3,5,7,9 => 5
-    //[11,17] => 11,13,15,17 => 4
-    int start=11, end=17;
-    int oddCounter=0;
-    while (start<=end)
-    {
-        if(start%2==1)
-        {
-            oddCounter++;
-        }
-        start++;
-    }
-    cout<<oddCounter<<endl;
+	double sum = 0, i = 1;
+	double avg = 0;
+	while (i <= 999)
+	{
+		sum = sum + i;
+		i++;
+	}
+	avg = sum / i;
+	cout << sum << endl;
+	cout << avg << endl;
 }
